077cd3f343daef5216d67a170c878bce7c2e84ec
jianlin <1270545331@qq.com>
2020-08-23 15:27:08 +0800

    预览界面显示问题

--- service/service-product/src/main/java/com/atguigu/gmall/product/test2.java
+++ service/service-product/src/main/java/com/atguigu/gmall/product/test2.java
@@ -11,5 +11,11 @@ public class test2 {
     public static void main(String[] args) {
      int b = 2;
      int a = 1;
+
+
+    }
+
+    public void test(){
+        System.out.println("111");
     }
 }
