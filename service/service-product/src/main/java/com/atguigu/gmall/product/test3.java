c3dcf85dc418acdaf9e40511e98f413faf21bb44
jianlin <1270545331@qq.com>
2020-08-23 18:23:59 +0800

    添加test3

---
+++ service/service-product/src/main/java/com/atguigu/gmall/product/test3.java
@@ -0,0 +1,15 @@
+package com.atguigu.gmall.product;
+
+/**
+ * test3
+ *
+ * @Author: DJL
+ * @CreateTime: 2020-08-23
+ * @Description:
+ */
+public class test3 {
+
+    public void aaa(){
+        System.out.println("sss");
+    }
+}
