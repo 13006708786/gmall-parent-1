ac774a947e4ccf4bb772ca9cee800497982a32f0
jianlin <1270545331@qq.com>
2020-08-23 18:45:36 +0800

    添加类5

---
+++ service/service-product/src/main/java/com/atguigu/gmall/product/test5.java
@@ -0,0 +1,16 @@
+package com.atguigu.gmall.product;
+
+/**
+ * test5
+ *
+ * @Author: DJL
+ * @CreateTime: 2020-08-23
+ * @Description:
+ */
+public class test5 {
+
+    public void cccc(){
+        System.out.println("cccc");
+    }
+
+}
